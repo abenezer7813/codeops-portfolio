ec4197640f3fb09c886ab91cb6f7652a4a1e8a29
abenezer7813 <abenimom1994@gmail.com>
2026-08-09 16:10:22 +0300

    add switch to payment service

--- module-02/day16/scripts/script.js
+++ module-02/day16/scripts/script.js
@@ -6,4 +6,16 @@ const partySize=5;
 const tip=bill>300?bill*0.1:bill*0.05;
 const total=bill+tip
 const perPerson=total/partySize
+
+switch (serviceProvider.toLowerCase()) {
+    case 'telebirr':
+        console.log("pay with telebirr")
+        break;
+    case 'cbebirr':
+       console.log('Pay with CBEBirr')
+        break;
+    default:
+        console.log('Pat With Other')
+        break;
+}
 console.log(`Total Bill ${total} ETB \nPer-Person ${perPerson} ETB`)
