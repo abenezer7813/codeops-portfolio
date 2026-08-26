15e8d13f351c3b105e9463a4d98b25c580b319b1
abenezer7813 <abenimom1994@gmail.com>
2026-08-26 16:26:08 +0300

    add static addis eats menu

--- module-03/day26/addis-eats/src/components/Main/Menu/Dish/Dish.jsx
+++ module-03/day26/addis-eats/src/components/Main/Menu/Dish/Dish.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Dish.css'
+const image ="../../../../assets/doro.png"
 function Dish(
     { dish }
 ) {
-    const { name, price, } = dish
+    const { name, price, description,spicy} = dish
     return (
         <div className='dish'>
-            <div>{name}</div>
-            <div>{price}</div>
+            {/* <div className='image'><img src='./doro.png' alt={name} /></div> */}
+            <div className='name'>{name}</div>
+            <div className='price'>{price} ETB</div>
+            {/* {spicy?<div className='spicy'>Spicy</div>:<div></div>}
+            <div className='description'>{description}</div> */}
         </div>
     )
 }
